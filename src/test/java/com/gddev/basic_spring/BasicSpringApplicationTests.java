40f87139348c9e841d8e6472a016ad747dbe26d6
gddev <dev.gustavodatovo@outlook.com>
2025-04-09 21:57:13 -0300

    Initial commit

---
+++ src/test/java/com/gddev/basic_spring/BasicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gddev.basic_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
